d60079f1552559f02facf305b9a1558b3637719e
jigglypop <ydh2244@gmail.com>
2022-03-24 00:37:21 +0900

    0324

--- lambda/defaultpackage.kt
+++
@@ -1,5 +0,0 @@
-fun main() {
-    val intro: String = "hello"
-    val num: Int = 20
-    println("intro: ${intro}, num: ${num}")
-}
---
+++ lambda/lambda.kt
@@ -0,0 +1,8 @@
+fun task1() {
+    println("start task1")
+    println("start task2")
+}
+
+fun main() {
+    task1()
+}
